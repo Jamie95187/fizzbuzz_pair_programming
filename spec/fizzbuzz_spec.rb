2a10aceb6a598c7a6e135ca6f565e03a3092705c
Jamie <jamiewong63@googlemail.com>
2019-11-05 15:33:18 +0000

    Added substance to method fizzbuzz.rb and added tests cases for buzz and fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,20 @@
 require 'fizzbuzz'
 #require './lib/fizzbuzz'
-#require_relative '../lib/fizzbuzz'
+# require_relative '../lib/fizzbuzz'
+
 describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
 end
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+end
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
+    
